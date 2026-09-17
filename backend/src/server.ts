a9f2a3e47f900f079acfe1b50aab3f040f84e5d0
Carlos Godoy - GFT <cgodoys.pex@sanitas.es>
2026-09-17 18:38:58 +0200

    fix express.json limit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ import curriculumRoutes from './routes/curriculum.routes';
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 import { runMigrations } from './migrations/runner';
 
